24da8d1ac85917bbe551bbab51ec3df79da7e22a
Herna Michel <labellehernamichel@yahoo.fr>
2019-06-03 17:57:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,9 +7,12 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
-    piglatinize = PigLatinizer.new
-    @latinized_word = "#{piglatinize.piglatinize(params[:user_phrase])}"
+    # piglatinize = PigLatinizer.new
+    # @latinized_word = "#{piglatinize.piglatinize(params[:user_phrase])}"
 
+    text_from_user = params[:user_phrase]
+    piglatinize_obj = PigLatinizer.new
+     @piglatinize_text = "#{piglatinize_obj.piglatinize(text_from_user)}"
     erb :pig_latinized
   end
 end
